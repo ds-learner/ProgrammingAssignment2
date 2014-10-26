df99f0cbd72d46a7cb7816d7ef483a96dc412331
ds-learner <joshua.rathmell@gmail.com>
2014-10-26 14:53:45 -0400

    Filled in body of the 2 function stubs provided by the course instructor.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These funtions allow the caching of a matrix's inverse
+## to avoid unnecessary re-computation.
 
-## Write a short comment describing this function
+
+## The function makeCacheMatrix() is a constructor 
+## whose output is a matrix m and a list of functions  
+## that can be called from another function (cacheSolve())
+## in order to cache the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ##Initialize the Cache matrix with NA values
+  dims <- dim(x)
+  nr <- dims[1]
+  nc <- dims[2]
+  
+  ncell <- nr * nc
+  
+  m <- matrix(data=NA, nrow= nr, ncol= nc)
+  
+  set <- function(y) {
+    x <<- y
+    m <<- matrix(data=NA, nrow= nr, ncol= nc)
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)  
 }
 
 
-## Write a short comment describing this function
+
+## The function cacheSolve takes as input
+## a matrix outputted by the makeCacheMatrix function
+## and outputs the matrix's inverse.
+## This function either computes the inverse or recalls
+## the cached inverse that was previously computed.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  
+  if (!anyNA(m)) {
+    message("Getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  
+  m <- solve(data, ...)
+  
+  x$setinv(m)
+  m
+  
 }
